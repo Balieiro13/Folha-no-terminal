e8f74cff875dc05c96d4c82072a93bb90da64329
Balieiro <balieiro1396@gmail.com>
2021-01-27 17:10:02 -0300

    New: adicionei a opção de ler a matéria direto no terminal

--- folha_mat.py
+++ folha_mat.py
@@ -27,6 +27,21 @@ def scrap_title_link():
         links.append(b[i]['href'])
     return links
 
+# Pega o conteúdo da matéria
+def scrap_article(n):
+    links = scrap_title_link()
+    news = requests.get(links[n])
+    news.encoding = 'UTF-8'
+    sopa = BeautifulSoup(news.text, 'html.parser')
+
+    article = sopa.find_all('p')
+    formated_lines = []
+    for arc in article:
+        x = str(arc)
+        if x.startswith('<p>'):
+            x = re.sub(r'<.*?>', '', x)
+            formated_lines.append(x.strip())
+    return formated_lines[:-2]
 
 def main():
     calls = scrap_title()
@@ -34,10 +49,15 @@ def main():
 
     try: c = int(sys.argv[1])
     except IndexError: c = None
+    try: n = sys.argv[2]
+    except IndexError: n = None
 
     # abre a n-ésima matéria caso tenha sido passado 'n' como arg
-    if c:
+    if c and not n:
         wb.open_new(links[c])
+    elif c and n:
+        for line in scrap_article(c):
+            print(line)
     else:
         for i in range(len(calls)):
             print(f'[{i}] ', calls[i])
